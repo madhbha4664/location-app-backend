2ce635d0d53a83e7612b1c553e57da1212707f5a
madhbha4664 <mr.madhbha4664@gmail.com>
2024-09-13 13:58:32 +0530

    Initial commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -43,5 +43,5 @@ app.post('/api/location', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
